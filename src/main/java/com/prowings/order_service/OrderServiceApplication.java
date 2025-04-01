e53eb7e4fba6800f5b1142fb672d0d8cd41a04fc
JD <jayprowings@gmail.com>
2025-04-01 11:13:55 +0530

    retry added

--- src/main/java/com/prowings/order_service/OrderServiceApplication.java
+++ src/main/java/com/prowings/order_service/OrderServiceApplication.java
@@ -2,8 +2,11 @@ package com.prowings.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
 
 @SpringBootApplication
+//@EnableCircuitBreaker
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/prowings/order_service/service/ValidationService.java
+++ src/main/java/com/prowings/order_service/service/ValidationService.java
@@ -8,35 +8,55 @@ import org.springframework.web.client.RestTemplate;
 import com.prowings.order_service.dto.RestaurantDto;
 import com.prowings.order_service.dto.UserDto;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
 public class ValidationService {
-	
+
 	@Autowired
-    private final RestTemplate restTemplate;
+	private final RestTemplate restTemplate;
 
-    @Value("${user.service.url}")
-    private String userServiceUrl;
+	@Value("${user.service.url}")
+	private String userServiceUrl;
 
-    @Value("${restaurant.service.url}")
-    private String restaurantServiceUrl;
+	@Value("${restaurant.service.url}")
+	private String restaurantServiceUrl;
 
-    public ValidationService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+	public ValidationService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
-    public UserDto validateUser(Long userId) {
-        String url = userServiceUrl + userId;
-        log.info("Validating user with URL: {} and userid : {}", url, userId);
-        return restTemplate.getForObject(url, UserDto.class);
-    }
+//	@CircuitBreaker(name = "userService", fallbackMethod = "userServiceFallback")
+	@Retry(name = "userService", fallbackMethod = "userServiceFallback")
+	public UserDto validateUser(Long userId) {
+		String url = userServiceUrl + userId;
+		log.info(">>>>>>>>> Validating user with URL: {} and userid : {}", url, userId);
+		return restTemplate.getForObject(url, UserDto.class);
+	}
 
-    public RestaurantDto validateRestaurant(Long restaurantId) {
-        String url = restaurantServiceUrl + restaurantId;
-        log.info("Validating restaurant with URL: {} and restaurantId : {}", url, restaurantId);
-        return restTemplate.getForObject(url, RestaurantDto.class);
-    }
+//	@CircuitBreaker(name = "restaurantService", fallbackMethod = "restaurantServiceFallback")
+	public RestaurantDto validateRestaurant(Long restaurantId) {
+		String url = restaurantServiceUrl + restaurantId;
+		log.info("Validating restaurant with URL: {} and restaurantId : {}", url, restaurantId);
+		return restTemplate.getForObject(url, RestaurantDto.class);
+	}
 
+	// Fallback Methods
+	public UserDto userServiceFallback(Long userId, Throwable throwable) {
+		
+		log.info("User Service is currently unavailable. Please try again later failure Reason : {}", throwable.getMessage());
+	
+		return null;
+		
+	}
+
+	public RestaurantDto restaurantServiceFallback(Long restaurantId, Throwable throwable) {
+		
+		log.info("Restaurant Service is currently down. Please try again later.");
+		
+		return null;
+	}
 }
